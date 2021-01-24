97fc9d64894ac13b6a8292c10b3d2178cdbec5aa
malasen <71284217+malasen@users.noreply.github.com>
2021-01-24 18:01:07 +0100

    6-8

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
-];
-
 
 // Question 1
 
@@ -35,14 +21,14 @@ heading.innerHTML = "Updated heading";
 heading.style.fontSize = "2em";
 
 
-//Question 4
+// Question 4
 
 heading.className = "subheading";
 
 console.dir(heading);
 
 
-//Question 5
+// Question 5
 
 const paragraphs = document.querySelectorAll ("p");
 
@@ -50,3 +36,62 @@ for (let i= 0; i < paragraphs.length; i++){
     console.dir(paragraphs[i]);    
     paragraphs[i].style.color = "red";
 }
+
+
+// Question 6
+
+const resultsContainer = document.querySelector (".results");
+console.dir(resultsContainer);
+
+resultsContainer.innerHTML = "<p>" + "New paragraph" + "</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+
+// Question 7
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function createList(list){
+    for (let i= 0; i < list.length; i++){
+        console.log(list[i].name);
+    }
+}
+createList(cats);
+
+
+// Question 8
+
+function createCats(cats){
+    let html = "";
+
+    for (let i= 0; i < cats.length; i++){
+
+        if(cats[i].age === undefined) {
+           cats[i].age = "Age Unknown";
+        }
+
+        html += `<h5> ${cats[i].name} </5> 
+        <p> ${cats[i].age} </p>` 
+        
+        console.log(html);
+    }
+    
+  return html;
+
+}
+createCats(cats);
+
+const container = document.querySelector(".cat-container");
+container.innerHTML = createCats(cats)
